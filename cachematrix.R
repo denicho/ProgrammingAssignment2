683b570faadf754fb190030f0951ad0f80f72ec8
denicho <deni.gavrilova@gmail.com>
2016-12-11 15:42:24 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## As in the given example for makeVector, I plan to create a special matrix
+## that contains a function to: set and get the value
+## of the matrix, and then set and get the inverse of the matrix 
 
-## Write a short comment describing this function
+## My aim is to create a special function - matrix that stores the value
+## of the input in a variable m.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+            m <- NULL
+            set <- function(y) {
+                    x <<- y
+                    m <<- NULL
+            }
+            get <- function() x
+            setinverse <- function(solve) m <<- solve
+            getinverse <- function() m
+            list(set = set, get = get,
+                 setinverse = setinverse,
+                 getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## I will create a function that computes the inverse of the matrix.
+## If the inverse is already calculated the function will get it from the cache
+## If the inverse has not been calculated, the matrix from makeCacheMatrix 
+## will be used, m will calculate the inverse and x$setinverse(m) will store
+## it in makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
